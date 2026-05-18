88536cc7baf9847d1a317db13158d705ba52d44b
Sohan Ac <sohanachhami655@gmail.com>
2026-05-18 13:55:46 +0545

    feat(admin): add routes for ownerdata,ownerbike, toggle, purchasebike and user bookings

--- backend/src/routes/admin.router.js
+++ backend/src/routes/admin.router.js
@@ -1,8 +1,13 @@
 import express from 'express' 
-import { adminLogin, userDataforAdmin } from '../controller/admin.controller.js'
+import { adminLogin, ownerBikeforAdmin, ownerDataforAdmin, purchaseBikeForAdmin, toggleBikeForAdmin, userBookingForAdmin, userDataforAdmin } from '../controller/admin.controller.js'
 import { isAdmin, isProtectedAdmin } from '../middleware/auth.admin.js'
 const adminRouter=express.Router() 
 
-adminRouter.get('/getuserdata',isProtectedAdmin,isAdmin,userDataforAdmin)
+adminRouter.get('/getuserdata',isProtectedAdmin,isAdmin,userDataforAdmin) 
+adminRouter.get('/getownerdata',isProtectedAdmin,isAdmin,ownerDataforAdmin)
+adminRouter.get('/getownerbike',isProtectedAdmin,isAdmin,ownerBikeforAdmin)
+adminRouter.patch('/togglebikeavailable',isProtectedAdmin,isAdmin,toggleBikeForAdmin)
+adminRouter.get('/purchasebike',isProtectedAdmin,isAdmin,purchaseBikeForAdmin)
+adminRouter.get('/userbookingdata',isProtectedAdmin,isAdmin,userBookingForAdmin)
 adminRouter.post('/login',adminLogin)
 export {adminRouter}
